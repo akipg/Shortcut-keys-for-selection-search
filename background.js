80570951e8d0fbf854812b0045aa05dc9607e3fb
sa-ji <108157287+sa-ji@users.noreply.github.com>
2022-07-10 16:19:34 +0900

    Add default search engine

--- background.js
+++ background.js
@@ -5,6 +5,12 @@ chrome.commands.onCommand.addListener(function(command) {
   else if (command == "search_selection_in_Google_2Background") {
     openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=false);
   }
+  else if (command == "search_selection_in_DefaultEngine_3NewTab") {
+    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=false);
+  }
+  else if (command == "search_selection_in_DefaultEngine_4Background") {
+    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=false);
+  }
 });
 
 function openSearchTab(baseURL_bef, baseURL_aft="", f_Active){
@@ -26,4 +32,26 @@ function openSearchTab(baseURL_bef, baseURL_aft="", f_Active){
     }    
   )
   return true
+}
+
+function openSearchTab_DefaultEngine(searchString, disposition){
+  chrome.tabs.executeScript(null, {
+    'code': 'encodeURI( document.selection ? document.selection.createRange().text\
+              : window.getSelection ? window.getSelection()\
+              : document.getSelection ? document.getSelection()\
+              : "" );'
+    },
+    function(results) {
+      if(!results)
+        return false
+      var searchString = decodeURI(results[0]);
+      var searchURL = baseURL_bef + searchString + baseURL_aft;
+      chrome.search.query({
+        text: searchString,
+        disposition
+      })
+    }    
+  )
+  return true
+
 }
