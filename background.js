d279b8970d85c48ab0936b792f80b4d8e21c71ef
sa-ji <108157287+sa-ji@users.noreply.github.com>
2022-09-20 17:16:49 +0900

    pdf search / manifest v3

--- background.js
+++ background.js
@@ -16,44 +16,141 @@ chrome.commands.onCommand.addListener(function (command) {
   }
 });
 
-function openSearchTab(baseURL_bef, baseURL_aft = "", f_Active) {
-  chrome.tabs.executeScript(null, {
-    'code': 'encodeURI( document.selection ? document.selection.createRange().text\
-              : window.getSelection ? window.getSelection()\
-              : document.getSelection ? document.getSelection()\
-              : "" );'
-  },
-    function (results) {
-      if (!results)
-        return false
-      var searchString = decodeURI(results[0]);
-      var searchURL = baseURL_bef + searchString + baseURL_aft;
-      chrome.tabs.create({
-        'url': searchURL,
-        'active': f_Active
-      });
+function getCurrentTabId(callback) {
+  console.log("getCurrentTabId", callback)
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    tabs => {
+      const tabId = tabs[0].id;
+      console.log("getCurrentTabId", "tabs", tabs, "tabId", tabId);
+      if (callback) {
+        callback(tabId);
+      }
     }
+  );
+}
+
+function getDOMSelectedText() {
+  return decodeURI(encodeURI(
+    document.selection ? document.selection.createRange().text
+      : window.getSelection ? window.getSelection()
+        : document.getSelection ? document.getSelection()
+          : ""
+  ));
+}
+
+function getPdfSelectedText() {
+  // get PDF selected text in chrome internal plugin
+  // refer to: https://stackoverflow.com/questions/61076303/how-can-i-get-selected-text-in-pdf-in-javascript
+  return new Promise(resolve => {
+    window.addEventListener('message', function onMessage(e) {
+      if (e.origin === 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai' &&
+        e.data && e.data.type === 'getSelectedTextReply') {
+        window.removeEventListener('message', onMessage);
+        resolve(e.data.selectedText);
+      }
+    });
+    // runs code in page context to access postMessage of the embedded plugin
+    const script = document.createElement('script');
+    if (chrome.runtime.getManifest().manifest_version > 2) {
+      script.src = chrome.runtime.getURL('query-pdf.js');
+    } else {
+      script.textContent = `(${() => {
+        document.querySelector('embed').postMessage({ type: 'getSelectedText' }, '*');
+      }})()`;
+    }
+    document.documentElement.appendChild(script);
+    script.remove();
+  });
+}
+
+function injection() {
+  console.log("injection")
+  // get DOM selected text
+  const domSelectionText = decodeURI(encodeURI(
+    document.selection ? document.selection.createRange().text
+      : window.getSelection ? window.getSelection()
+        : document.getSelection ? document.getSelection()
+          : ""
+  ));
+  console.log("injection", "domSelectionText", domSelectionText);
+  if (domSelectionText) {
+    console.log("injection", "return domSelectionText", domSelectionText);
+    return new Promise(resolve => resolve(domSelectionText));
+  }
+  else if(document.querySelector('embed')){
+
+    // get PDF selected text in chrome internal plugin
+    // refer to: https://stackoverflow.com/questions/61076303/how-can-i-get-selected-text-in-pdf-in-javascript
+    console.log("PDFINJECTION")
+    return new Promise(resolve => {
+      window.addEventListener('message', function onMessage(e) {
+        if (e.origin === 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai' &&
+          e.data && e.data.type === 'getSelectedTextReply') {
+          window.removeEventListener('message', onMessage);
+          resolve(e.data.selectedText);
+        }
+      });
+      // runs code in page context to access postMessage of the embedded plugin
+      const script = document.createElement('script');
+      if (chrome.runtime.getManifest().manifest_version > 2) {
+        script.src = chrome.runtime.getURL('query-pdf.js');
+      } else {
+        script.textContent = `(${() => {
+          document.querySelector('embed').postMessage({ type: 'getSelectedText' }, '*');
+        }})()`;
+      }
+      document.documentElement.appendChild(script);
+      script.remove();
+    });
+
+  }
+  else{
+    return "";
+  }
+}
+
+
+function getSelectedText(tabId, callback) {
+  console.log("getSelectedText", "tabId", tabId, "callback", callback);
+  chrome.scripting.executeScript(
+    {
+      //target: { tabId, allFrames: true },
+      target: { tabId, allFrames: false },
+      func: injection,
+    },
+    injectionResults => {
+      console.log("getSelectedText", "injectionaResults", injectionResults);
+      const text = injectionResults[0].result;
+      callback(text);
+    }
+  );
+}
+
+function openSearchTab(baseURL_bef, baseURL_aft = "", f_Active) {
+  console.log("openSearchTab", baseURL_bef, baseURL_aft = "", f_Active)
+  getCurrentTabId(
+    tabId => getSelectedText(tabId,
+      text => {
+        console.log("openSearchTab", "text", text);
+        var searchURL = baseURL_bef + text + baseURL_aft;
+        chrome.tabs.create({
+          'url': searchURL,
+          'active': f_Active
+        });
+      }
+    )
   )
-  return true
+  return true;
 }
 
 function openSearchTab_DefaultEngine(disposition) {
-  chrome.tabs.executeScript(null, {
-    'code': 'encodeURI( document.selection ? document.selection.createRange().text\
-              : window.getSelection ? window.getSelection()\
-              : document.getSelection ? document.getSelection()\
-              : "" );'
-  },
-    function (results) {
-      if (!results)
-        return false
-      var searchString = decodeURI(results[0]);
+  getCurrentTabId(tabId => getSelectedText(tabId,
+    text => {
       chrome.search.query({
-        text: searchString,
+        text,
         disposition
       })
-    }
-  )
-  return true
-
+    }));
+  return true;
 }
