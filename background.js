90c2bd79cc110d8437727855d3e09cbb95937437
sa-ji <108157287+sa-ji@users.noreply.github.com>
2022-09-20 14:04:22 +0900

    Prettier background.js

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.commands.onCommand.addListener(function(command) {
-    if (command == "search_selection_in_Google_1Foreground") {
-    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=true);
+chrome.commands.onCommand.addListener(function (command) {
+  if (command == "search_selection_in_Google_1Foreground") {
+    openSearchTab(baseURL_bef = "https://www.google.com/search?q=", baseURL_aft = "", f_Active = true);
   }
   else if (command == "search_selection_in_Google_2Background") {
-    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=false);
+    openSearchTab(baseURL_bef = "https://www.google.com/search?q=", baseURL_aft = "", f_Active = false);
   }
   else if (command == "search_selection_in_DefaultEngine_3NewTab") {
     openSearchTab_DefaultEngine("NEW_TAB");
@@ -16,43 +16,43 @@ chrome.commands.onCommand.addListener(function(command) {
   }
 });
 
-function openSearchTab(baseURL_bef, baseURL_aft="", f_Active){
+function openSearchTab(baseURL_bef, baseURL_aft = "", f_Active) {
   chrome.tabs.executeScript(null, {
     'code': 'encodeURI( document.selection ? document.selection.createRange().text\
               : window.getSelection ? window.getSelection()\
               : document.getSelection ? document.getSelection()\
               : "" );'
-    },
-    function(results) {
-      if(!results)
+  },
+    function (results) {
+      if (!results)
         return false
       var searchString = decodeURI(results[0]);
       var searchURL = baseURL_bef + searchString + baseURL_aft;
       chrome.tabs.create({
-        'url':searchURL,
-        'active':f_Active
+        'url': searchURL,
+        'active': f_Active
       });
-    }    
+    }
   )
   return true
 }
 
-function openSearchTab_DefaultEngine(disposition){
+function openSearchTab_DefaultEngine(disposition) {
   chrome.tabs.executeScript(null, {
     'code': 'encodeURI( document.selection ? document.selection.createRange().text\
               : window.getSelection ? window.getSelection()\
               : document.getSelection ? document.getSelection()\
               : "" );'
-    },
-    function(results) {
-      if(!results)
+  },
+    function (results) {
+      if (!results)
         return false
       var searchString = decodeURI(results[0]);
       chrome.search.query({
         text: searchString,
         disposition
       })
-    }    
+    }
   )
   return true
 
