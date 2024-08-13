46a0063ad381d3f9101bf05ec6e3fadd933284c8
akipg <108157287+akipg@users.noreply.github.com>
2024-08-13 12:31:33 +0900

    Update to manifest v3

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.commands.onCommand.addListener(function(command) {
     if (command == "search_selection_in_Google_1Foreground") {
-    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=true);
+    openSearchTab("https://www.google.com/search?q=", "",  true);
   }
   else if (command == "search_selection_in_Google_2Background") {
-    openSearchTab(baseURL_bef="https://www.google.com/search?q=", baseURL_aft="",  f_Active=false);
+    openSearchTab("https://www.google.com/search?q=", "",  false);
   }
   else if (command == "search_selection_in_DefaultEngine_3NewTab") {
     openSearchTab_DefaultEngine("NEW_TAB");
@@ -16,44 +16,49 @@ chrome.commands.onCommand.addListener(function(command) {
   }
 });
 
+async function getCurrentTab(){
+  const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+  return tabs[0];
+}
+
+async function getSelectedTextInAllFrames(){
+  const tab = await getCurrentTab();
+  const results = await chrome.scripting.executeScript({
+      target: {tabId: tab.id, allFrames: true},
+      func: function(){
+        return encodeURI(
+          document.selection ? document.selection.createRange().text :
+          window.getSelection ? window.getSelection() :
+          document.getSelection ? document.getSelection() :
+          ""
+        );
+      }
+    })
+    return results;
+}
+
 function openSearchTab(baseURL_bef, baseURL_aft="", f_Active){
-  chrome.tabs.executeScript(null, {
-    'code': 'encodeURI( document.selection ? document.selection.createRange().text\
-              : window.getSelection ? window.getSelection()\
-              : document.getSelection ? document.getSelection()\
-              : "" );'
-    },
-    function(results) {
-      if(!results)
-        return false
-      var searchString = decodeURI(results[0]);
-      var searchURL = baseURL_bef + searchString + baseURL_aft;
-      chrome.tabs.create({
-        'url':searchURL,
-        'active':f_Active
-      });
-    }    
-  )
+  getSelectedTextInAllFrames().then(results => {
+    if(!results) return false;
+    var searchString = decodeURI(results[0].result);
+    var searchURL = baseURL_bef + searchString + baseURL_aft;
+    chrome.tabs.create({
+      'url':searchURL,
+      'active':f_Active
+    });
+  });   
   return true
 }
 
 function openSearchTab_DefaultEngine(disposition){
-  chrome.tabs.executeScript(null, {
-    'code': 'encodeURI( document.selection ? document.selection.createRange().text\
-              : window.getSelection ? window.getSelection()\
-              : document.getSelection ? document.getSelection()\
-              : "" );'
-    },
-    function(results) {
-      if(!results)
-        return false
-      var searchString = decodeURI(results[0]);
-      chrome.search.query({
-        text: searchString,
-        disposition
-      })
-    }    
-  )
+  getSelectedTextInAllFrames().then(results => {
+    if(!results) return false;
+    var searchString = decodeURI(results[0].result);
+    chrome.search.query({
+      text: searchString,
+      disposition
+    })
+  });
   return true
 
 }
